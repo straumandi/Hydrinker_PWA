93ab08d3136ec7dcab0d1cfa5254fdadecdd046d
straumandi <straumandi@gmail.com>
2023-12-24 02:03:14 +0100

    create bottom nav bar and according screen components, extract navbar to component still todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import { BottomNavbarComponent } from './components/bottom-navbar/bottom-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, BottomNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
