ba2e468c61ec0be8328b243730fc0f6e81a508ca
straumandi <straumandi@gmail.com>
2023-12-24 15:24:55 +0100

    add bottom nav bar component to root

--- src/app/components/bottom-navbar/bottom-navbar.component.ts
+++ src/app/components/bottom-navbar/bottom-navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-bottom-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './bottom-navbar.component.html',
   styleUrl: './bottom-navbar.component.css'
 })
