4f0c540c529faff7fe0f14c36ccb0dd8032a5beb
Benedikt Schmatz <benedikt.schmatz@edu.fh-joanneum.at>
2023-12-25 12:46:27 +0100

    added profile screen

--- src/app/components/profile/profile/profile.component.ts
+++ src/app/components/profile/profile/profile.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { LocalStorageService } from '../../../services/local-storage/local-storage.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profileForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private localStorageService: LocalStorageService,
+  ) {}
+
+  ngOnInit() {
+    const existingProfile = this.localStorageService.getProfile(); // Adjust the method to match your service
+    this.profileForm = this.formBuilder.group({
+      name: [existingProfile?.name || '', Validators.required],
+      weight: [existingProfile?.weight || '', Validators.required],
+      gender: [existingProfile?.gender || 'Male', Validators.required],
+      dailyGoal: [existingProfile?.dailyGoal || '', Validators.required],
+      age: [existingProfile?.age || '', Validators.required],
+      drinkSize: [existingProfile?.drinkSize || '', Validators.required],
+    });
+  }
+
+  saveProfile() {
+    if (this.profileForm.valid) {
+      this.localStorageService.saveProfile(this.profileForm.value); // Adjust the method to match your service
+    }
+  }
 }
