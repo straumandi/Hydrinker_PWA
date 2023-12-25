5f0f18da083d66328f9951b5504f6fd4e87e421c
straumandi <straumandi@gmail.com>
2023-12-25 17:26:22 +0100

    add hydration-service incl. HydrationData model  and angular material to project

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { HydrationService } from '../../../services/hydration-service/hydration.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  drinkForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private hydrationService: HydrationService) {
+    this.drinkForm = this.formBuilder.group({
+      drinkSize: [0, Validators.required],
+    });
+  }
+
+  addDrink(): void {
+    const drinkSize = this.drinkForm.value.drinkSize;
+    this.hydrationService.addDrink(drinkSize);
+    this.drinkForm.reset();
+  }
 }
