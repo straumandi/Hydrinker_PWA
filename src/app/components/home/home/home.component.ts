aa2329cebfc6e5e1d16852a765cd747094ae73f3
Benedikt Schmatz <benedikt.schmatz1@gmail.com>
2023-12-29 22:06:18 +0100

    fix default size

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HydrationService } from '../../../services/hydration-service/hydration.service';
 import { DialogService } from '../../../services/dialog-service/dialog.service';
 import { ScoreService } from '../../../services/score/score.service';
+import { LocalStorageService } from '../../../services/local-storage/local-storage.service';
 
 @Component({
   selector: 'app-home',
@@ -18,25 +14,19 @@ import { ScoreService } from '../../../services/score/score.service';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  drinkForm: FormGroup;
   score = 0;
 
   constructor(
-    private formBuilder: FormBuilder,
     private hydrationService: HydrationService,
     private dialogService: DialogService,
     protected scoreService: ScoreService,
+    private localStorageService: LocalStorageService,
   ) {
-    this.drinkForm = this.formBuilder.group({
-      drinkSize: [0, Validators.required],
-    });
     this.updateScore();
   }
 
-  addDrink(): void {
-    const drinkSize = +this.drinkForm.value.drinkSize;
+  addDrink(drinkSize: number): void {
     this.hydrationService.addDrink(drinkSize);
-    this.drinkForm.reset();
     this.updateScore();
   }
 
@@ -47,9 +37,23 @@ export class HomeComponent {
   openDrinkSizeDialog() {
     this.dialogService.openDrinkSizeDialog().subscribe((result) => {
       if (result) {
-        this.drinkForm.patchValue({ drinkSize: +result });
-        this.addDrink();
+        this.addDrink(+result);
       }
     });
+  }
+
+  addDrinkFromForm() {
+    const profile = this.localStorageService.getProfile();
+    if (!profile) {
+      return;
+    }
+
+    if (!profile.drinkSize) {
+      this.openDrinkSizeDialog();
+      return;
+    }
+
+    const drinkSize = +profile.drinkSize;
+    this.addDrink(drinkSize);
   }
 }
