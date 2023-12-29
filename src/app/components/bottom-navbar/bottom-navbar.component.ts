d473975c3bb463bcc9c40f6a323d6803ce9ce5bb
Benedikt Schmatz <benedikt.schmatz1@gmail.com>
2023-12-29 16:36:22 +0100

    added import

--- src/app/components/bottom-navbar/bottom-navbar.component.ts
+++ src/app/components/bottom-navbar/bottom-navbar.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-bottom-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './bottom-navbar.component.html',
-  styleUrl: './bottom-navbar.component.css'
+  styleUrl: './bottom-navbar.component.css',
 })
-export class BottomNavbarComponent {
-
-}
+export class BottomNavbarComponent {}
